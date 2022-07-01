3db3ef24a5444e67f2441ee3a04594e0fddbe85e
LXiaoMiao <1416887647@qq.com>
2022-07-01 18:29:26 +0800

    1.0

--- src/main/java/com/example/springboot/web/OrderController.java
+++ src/main/java/com/example/springboot/web/OrderController.java
@@ -7,11 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -23,15 +20,13 @@ public class OrderController {
     SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     @RequestMapping("/placeOrder")
-    public String placeOrder(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public String placeOrder(HttpServletRequest request){
         HttpSession session = request.getSession();
         Double amount = (Double) session.getAttribute("amount");
         Integer gid = (Integer) session.getAttribute("gid");
         User user = (User) session.getAttribute("user");
 
         if (user == null){
-//            response.sendRedirect("/register/login.html");
-            System.out.println("知道");
             return "register/login";
         }
         else{
@@ -42,14 +37,32 @@ public class OrderController {
             order.setOrderdate(ft.format(orderdate.getTime()));
             order.setGoodstable_id(gid);
             orderMapper.placeOrder(order);
-//            response.sendRedirect("/shop/success.html");
-            System.out.println("不知道");
             return "shop/success";
         }
     }
 
     @RequestMapping("/addCart")
-    public String addCart(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public String addCart(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        Integer gid = (Integer) session.getAttribute("gid");
+        User user = (User) session.getAttribute("user");
+
+        if (user == null){
+//            response.sendRedirect("/register/login.html");
+            return "register/login";
+        }
+        else{
+            Order order = new Order();
+            order.setBusertable_id(user.getId());
+            order.setGoodstable_id(gid);
+            orderMapper.addCart(order);
+//            response.sendRedirect("/shop/success.html");
+            return "shop/success";
+        }
+    }
+
+    @RequestMapping("/findOrder")
+    public String findOrder(HttpServletRequest request){
         HttpSession session = request.getSession();
         Integer gid = (Integer) session.getAttribute("gid");
         User user = (User) session.getAttribute("user");
